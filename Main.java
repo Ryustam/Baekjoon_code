b26283f22aec55dec5c42aafb5a7e2f1fd7b04b4
Ryustam <88639758+Ryustam@users.noreply.github.com>
2022-03-17 18:02:26 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int decks = Integer.parseInt(bf.readLine());
+
+        PriorityQueue<Integer> priorityQueueLowest = new PriorityQueue<>();
+
+        for (int i=0 ; i < decks; i++ ){
+            priorityQueueLowest.add(Integer.parseInt(bf.readLine()));
+        }
+
+        int total=0;
+        int one = 0;
+        int two = 0;
+        while(priorityQueueLowest.peek() != null){
+            if(priorityQueueLowest.size()==1){
+                total += priorityQueueLowest.poll();
+                break;
+            }
+            one = priorityQueueLowest.poll();
+            two = priorityQueueLowest.poll();
+            total += one + two ;
+            priorityQueueLowest.add(one+two);
+        }
+
+        System.out.println(total);
+    }
+}
